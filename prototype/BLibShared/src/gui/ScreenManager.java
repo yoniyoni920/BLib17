e9452a6efda326a4da1d07471744256410050db9
Luffy <luffydafloffi@gmail.com>
2024-12-20 22:34:39 +0200

    Add some comments to screen manager

--- prototype/BLibShared/src/gui/ScreenManager.java
+++ prototype/BLibShared/src/gui/ScreenManager.java
@@ -23,6 +23,15 @@ public class ScreenManager {
 		this.primaryStage = primaryStage;
 	}
 
+	/**
+	 * Receives screenName of a screen (gui/{screenName}.fxml), loads it and returns the AbstractScreen associated with it
+	 * AbstractScreen is basically a JavaFX controller
+	 * 
+	 * @param screenName
+	 * @param title
+	 * @return
+	 * @throws IOException
+	 */
 	public AbstractScreen openScreen(String screenName, String title) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/" + screenName + ".fxml"));
 
@@ -43,6 +52,13 @@ public class ScreenManager {
 		return screen;
 	}
 	
+	/**
+	 * Closes the screen and returns the previous screen that was open
+	 * NOTE: if the last screen contained information it will still be loaded.
+	 * You must handle this.
+	 * 
+	 * @return
+	 */
 	public AbstractScreen closeScreen() {
 		AbstractScreen screen = screens.pop();
 		AbstractScreen lastScreen = screens.lastElement();
@@ -53,6 +69,10 @@ public class ScreenManager {
 		return lastScreen;
 	}
 	
+	/**
+	 * Sets the screen. This is internally used.
+	 * @param screen
+	 */
 	private void setScreen(AbstractScreen screen) {
 		primaryStage.setTitle(screen.getTitle());
 		primaryStage.setScene(screen.getScene());

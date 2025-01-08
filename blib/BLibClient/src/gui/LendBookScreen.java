ad9e7af6eeaa9d774e39672a3c0dd4e0de20d3a9
Khillo <khalilwarwar2@gmail.com>
2025-01-08 14:18:58 +0200

    Configure date pickers acceptable dates
    also hid labels that need to be hidden

--- blib/BLibClient/src/gui/LendBookScreen.java
+++ blib/BLibClient/src/gui/LendBookScreen.java
@@ -4,11 +4,45 @@ import controllers.BookScanner;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.time.LocalDate;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
+
 
 public class LendBookScreen extends AbstractScreen {
 	@FXML
 	TextField bookIdTextField;
+	@FXML
+	Label noBookCopyErrLabel;
+	@FXML
+	Label subFrozenErrLabel;
+	@FXML
+	DatePicker returnDatePicker;
+	@FXML
+	DatePicker lendDatePicker;
 
+	@FXML
+	public void initialize() {
+		lendDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+			returnDatePicker.setDayCellFactory(param -> new DateCell() {
+				@Override
+				public void updateItem(LocalDate item, boolean empty) {
+					super.updateItem(item, empty);
+					setDisable(item.isAfter(lendDatePicker.getValue().plusWeeks(2)) || item.isBefore(lendDatePicker.getValue()));
+				}
+			});
+			returnDatePicker.setValue(lendDatePicker.getValue().plusWeeks(2));
+		});
+		lendDatePicker.setDayCellFactory(param -> new DateCell() {
+			@Override
+			public void updateItem(LocalDate item, boolean empty) {
+				super.updateItem(item, empty);
+				setDisable(item.isBefore(LocalDate.now()));
+			}
+		});
+
+		lendDatePicker.setValue(LocalDate.now());
+	}
 
 	public void searchBooskByName() {
 		// TODO - implement LendBookScreen.searchBooskByName

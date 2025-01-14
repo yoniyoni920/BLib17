f916e4a86e4c1058e9e4b12dcb26405d550c52da
Luffy <luffydafloffi@gmail.com>
2025-01-14 04:59:27 +0200

    Async sendMessage

--- blib/BLibClient/src/services/ClientUtils.java
+++ blib/BLibClient/src/services/ClientUtils.java
@@ -3,6 +3,9 @@ package services;
 import base.Action;
 import base.ClientApplication;
 import entities.Message;
+import javafx.application.Platform;
+
+import java.util.function.Consumer;
 
 /**
  * This is a utility class that is used for simplifying communication
@@ -35,5 +38,19 @@ public class ClientUtils {
      */
     public static Message sendMessage(Action action, Object object) {
         return sendMessage(new Message(action, object));
+    }
+
+    /**
+     * Sends a message in a different thread, this is useful if you call this is many times and don't wish to slowdown
+     * the UI thread.
+     * @param msg
+     * @param then
+     */
+    public static void sendMessage(Message msg, Consumer<Message> then) {
+        Thread t = new Thread(() -> {
+            Message result = sendMessage(msg);
+            Platform.runLater(() -> then.accept(result));
+        });
+        t.start();
     }
 }

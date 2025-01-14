c3bbc2482c5aa4db4122e0f767edc35727c62bba
Luffy <luffydafloffi@gmail.com>
2025-01-14 04:59:59 +0200

    Improved search books screen

--- blib/BLibClient/src/gui/BookCard.java
+++ blib/BLibClient/src/gui/BookCard.java
@@ -5,10 +5,15 @@ import java.net.URISyntaxException;
 import entities.Book;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class BookCard {
+    @FXML
+    private AnchorPane anchorPane;
+
 	@FXML
     private ImageView bookImageView;
     @FXML
@@ -32,5 +37,10 @@ public class BookCard {
 	    if (image.isError()) {
 	         System.out.println("Error loading image: " + image.getException().getMessage());
 	    }
+
+        Tooltip tooltip = new Tooltip(book.getDescription());
+        tooltip.setWrapText(true);
+        tooltip.setWidth(200);
+        Tooltip.install(anchorPane, tooltip);
     }
 }
--- blib/BLibClient/src/gui/SearchBooksScreen.java
+++ blib/BLibClient/src/gui/SearchBooksScreen.java
@@ -3,10 +3,9 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 import base.Action;
 import base.ClientApplication;
@@ -23,12 +22,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.InputEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -38,6 +33,9 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
+import javafx.util.converter.FormatStringConverter;
+import services.ClientUtils;
 
 
 public class SearchBooksScreen extends AbstractScreen implements Initializable{
@@ -51,67 +49,113 @@ public class SearchBooksScreen extends AbstractScreen implements Initializable{
 	private TextField searchId;
 	
 	@FXML
-	private ChoiceBox choiceBox;
+	private ChoiceBox<String> choiceBox;
 	
 	@FXML
 	private Text textId;
-	
-	public void onSearch(ActionEvent event) throws Exception{
-		searchBooks();
-	}
-	public void searchBooks() {
-		int row = 0, col = 0;
-		textId.setVisible(false);
-		String[] searchInfo = {searchId.getText() ,(String)choiceBox.getValue()};
-		gridPane.getChildren().clear();
-		gridPane.getRowConstraints().clear(); // Clear previous row constraints
-	    gridPane.getColumnConstraints().clear();
-		Message msg = ClientApplication.chat.sendToServer(new Message(Action.SEARCH_BOOKS, searchInfo));
-		System.out.println("Received object type: " + msg.getObject().getClass().getName());
-		List<Book> books =  (List<Book>) msg.getObject();
-		if(!books.isEmpty()) {
-			RowConstraints rc = new RowConstraints();
-			double rowCount = Math.ceil((double)books.size()/3);
-			rc.setPercentHeight(100 / rowCount);
 
-			for (int i = 0; i < rowCount; i++) {
-		    	gridPane.getRowConstraints().add(rc);
-			}
+	@FXML
+	private ScrollPane scrollPane;
 
-			ColumnConstraints cc = new ColumnConstraints();
-				cc.setPercentWidth(100d / 3);
-		
-			for (int i = 0; i < 3; i++) {
-		    	gridPane.getColumnConstraints().add(cc);
-			}
-			for(Book book : books ) {
-				try {
-				 	FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/BookCard.fxml"));
-				 	Node card = loader.load();
-				 	GridPane.setVgrow(card, Priority.ALWAYS);
-				 	BookCard bookCard = loader.getController();
-				 	bookCard.setBookData(book);
-				 	gridPane.add(card, col, row);
-				 	col++;
-				 	if (col == 3) { // 3 cards per row
-	                    	col = 0;
-	                    	row++;
-
-	                }
-				}catch(IOException e) {
-                	e.printStackTrace();
-            	}
-			}
-		}
-		else {
-			textId.setVisible(true);
-		}
-	}
+	private Timer timer;
+	private TimerTask searchTask;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		timer = new Timer();
+
 		choiceBox.setItems(choiceBoxList);
 		choiceBox.setValue("title");
+
+		// Convert the values to be capitalized rather than lowercase
+		choiceBox.setConverter(new StringConverter<String>() {
+			@Override
+			public String fromString(String str) {
+				return str.toLowerCase();
+			}
+
+			@Override
+			public String toString(String str) {
+				return  str.substring(0, 1).toUpperCase() + str.substring(1);
+			}
+		});
 		searchBooks();
+
+		// Lets you speed up the scroll speed
+		final double SCROLL_SPEED = 0.014;
+		scrollPane.getContent().setOnScroll(scrollEvent -> {
+			double deltaY = scrollEvent.getDeltaY() * SCROLL_SPEED;
+			scrollPane.setVvalue(scrollPane.getVvalue() - deltaY);
+		});
+	}
+
+	public void onSearch(InputEvent event) throws Exception {
+		// Makes search faster
+		if (searchTask != null) {
+			searchTask.cancel();
+		}
+		searchTask = new TimerTask() {
+			@Override
+			public void run() {
+				searchBooks();
+			}
+		};
+		timer.schedule(searchTask, 200);
+	}
+
+	public void searchBooks() {
+		String[] searchInfo = {searchId.getText(), choiceBox.getValue()};
+
+		ClientUtils.sendMessage(new Message(Action.SEARCH_BOOKS, searchInfo), msg -> {
+			textId.setVisible(false);
+
+			int definedColumns = 4;
+			int row = 0, col = 0;
+
+			// Clear previous row constraints
+			gridPane.getChildren().clear();
+			gridPane.getRowConstraints().clear();
+			gridPane.getColumnConstraints().clear();
+
+			System.out.println("Received object type: " + msg.getObject().getClass().getName());
+			List<Book> books = (List<Book>) msg.getObject();
+			if(!books.isEmpty()) {
+				RowConstraints rc = new RowConstraints();
+				double rowCount = Math.ceil((double)books.size()/definedColumns);
+				rc.setPercentHeight(100 / rowCount);
+
+				for (int i = 0; i < rowCount; i++) {
+					gridPane.getRowConstraints().add(rc);
+				}
+
+				ColumnConstraints cc = new ColumnConstraints();
+				cc.setPercentWidth(100 / definedColumns);
+
+				for (int i = 0; i < definedColumns; i++) {
+					gridPane.getColumnConstraints().add(cc);
+				}
+
+				for(Book book : books) {
+					try {
+						FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/BookCard.fxml"));
+						Node card = loader.load();
+						GridPane.setVgrow(card, Priority.ALWAYS);
+						BookCard bookCard = loader.getController();
+						bookCard.setBookData(book);
+						gridPane.add(card, col, row);
+						col++;
+						if (col == definedColumns) { // definedColumns cards per row
+							col = 0;
+							row++;
+						}
+					} catch(IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			else {
+				textId.setVisible(true);
+			}
+		});
 	}
 }

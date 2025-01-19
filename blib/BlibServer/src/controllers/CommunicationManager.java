5fc0a04b64fd8ddd7ca03ae87b569135359767b5
Khillo <khalilwarwar2@gmail.com>
2025-01-19 23:18:08 +0200

    Added send email funcionality

--- blib/BlibServer/src/controllers/CommunicationManager.java
+++ blib/BlibServer/src/controllers/CommunicationManager.java
@@ -1,6 +1,8 @@
 package controllers;
 
 
+import sun.net.www.http.HttpClient;
+
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
@@ -8,9 +10,54 @@ import java.util.Base64;
 
 public class CommunicationManager {
 
-	public static void sendMail() {
-		// TODO - implement CommunicationManager.sendMail
-		throw new UnsupportedOperationException();
+	/**
+	 *	Function to send email using mailersend api
+	 * @param to Recipient email
+	 * @param subject Email subject
+	 * @param body Email body
+	 * @param senderName The name to be shown as the email from
+	 */
+	public static void sendMail(String to, String subject, String body, String senderName) {
+		final String token = "mlsn.d4074bb922cfb8f612862b99462eb3753b364d57bf01dae6c232d34e034f83da";
+		try {
+
+			URL url = new URL("https://api.mailersend.com/v1/email");
+			URLConnection con = url.openConnection();
+			HttpURLConnection http = (HttpURLConnection) con;
+			http.setRequestMethod("POST");
+			http.setRequestProperty("Content-Type", "application/json");
+			http.addRequestProperty("Authorization", "Bearer " + token);
+			http.addRequestProperty("User-Agent", "Application");
+			http.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(http.getOutputStream());
+			OutputStreamWriter writer = new OutputStreamWriter(wr);
+			writer.write("{" +
+					"\"from\":{\"email\":\"Blib@trial-0r83ql39nzmlzw1j.mlsender.net\",\"name\":\"" + senderName + "\"}," +
+					"\"to\":[{\"email\":\"" + to + "\"}]," +
+					"\"subject\":\"" + subject + "\"," +
+					"\"text\":\"" + body + "\"" +
+					"}");
+			writer.flush();
+			writer.close();
+			http.connect();
+
+			//get output data and print if error
+			if(http.getResponseCode() != 202) {
+				String result;
+				BufferedInputStream bis = 100 <= http.getResponseCode() && http.getResponseCode() <= 399?new BufferedInputStream(http.getInputStream()):
+						new BufferedInputStream(http.getErrorStream());
+				ByteArrayOutputStream buf = new ByteArrayOutputStream();
+				int result2 = bis.read();
+				while (result2 != -1) {
+					buf.write((byte) result2);
+					result2 = bis.read();
+				}
+				result = buf.toString();
+				System.out.println(result);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	/**
